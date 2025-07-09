3aceeaf0e654d9815d13c7a5f0699349ad4dc7f5
n <n@n.n>
2025-07-09 13:40:23 +0200

    se añadieron las funciones restar,dividir,multiplicar y reiniciarTotal

--- src/main/java/es/cic/curso25/Calculadora.java
+++ src/main/java/es/cic/curso25/Calculadora.java
@@ -7,7 +7,27 @@ public class Calculadora extends Object {
         return total;
     }
 
+    public void reiniciarTotal(){
+        total =0;
+    }
+
     public void sumar(double valor) {
         total = total + valor;
+    }
+
+    public void restar(double valor) {
+        total = total - valor;
+    }
+
+     public void multiplicar(double valor) {
+        total = total * valor;
+    }
+
+    public void dividir(double valor) {
+        try {
+            total = total /valor;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/es/cic/curso25/CalculadoraTest.java
+++ src/test/java/es/cic/curso25/CalculadoraTest.java
@@ -5,12 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
+
+    // Preparo
+    private Calculadora cut = new Calculadora();
+
     @Test
     public void testSumar() {
-        // Preparo
-        Calculadora cut = new Calculadora();
-
-
+        cut.reiniciarTotal();
         // Ejecuto
         cut.sumar(5.6);
 
@@ -18,10 +19,37 @@ public class CalculadoraTest {
         double valorActual = cut.getTotal();
         assertEquals(5.6, valorActual, 0.000001);
 
-
         cut.sumar(3.4);
         valorActual = cut.getTotal();
 
         assertEquals(9, valorActual, 0.000001);
     }
+
+    @Test
+    public void restar() {
+        cut.reiniciarTotal();
+        cut.restar(3);
+        double valorActual =  cut.getTotal();
+        assertEquals(valorActual, -3);
+
+    }
+
+    @Test
+    public void dividir() {
+        cut.reiniciarTotal();
+        cut.sumar(15);
+        cut.dividir(3);
+        double valorActual =  cut.getTotal();
+        assertEquals(valorActual, 5);
+    }
+
+    @Test
+    public void multiplicar() {
+        cut.reiniciarTotal();
+        cut.sumar(5);
+        cut.multiplicar(3);
+        double valorActual =  cut.getTotal();
+        assertEquals(valorActual, 15);
+    }
+
 }
